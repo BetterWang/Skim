4f8d4513d9d425d40a22d7881eb78eb8476c4fc3
Quan Wang <BetterWang@gmail.com>
2016-01-18 17:53:13 +0100

    [+] MB and HP skim.

---
+++ crab_HIMinBias_HIRun2015-PromptReco-v2_262548-262768.py
@@ -0,0 +1,56 @@
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+from CRABAPI.RawCommand import crabCommand
+from CRABClient.ClientExceptions import ClientException
+from httplib import HTTPException
+
+config = config()
+
+config.General.requestName = 'HIMinimumBias1_HIRun2015-PromptReco-v2_262548-262768'
+config.General.workArea = 'CrabArea'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'skimHIMB_v1.py'
+
+config.Data.inputDataset = '/HIMinimumBias1/HIRun2015-PromptReco-v1/AOD'
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'LumiBased'
+config.Data.unitsPerJob = 5
+#config.Data.outLFNDirBase = '/store/group/phys_heavyions/qwang/Skim2015/'
+config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
+#config.Data.outLFNDirBase = '/store/user/%s/HIHardProbes_HIRun2015-PromptReco-v1_FullTrack_v1' % (getUsernameFromSiteDB())
+#config.Data.outLFNDirBase = '/store/group/phys_heavyions/qwang/'
+config.Data.lumiMask = 'Cert_262548-262768_PromptReco_HICollisions15_JSON.txt'
+#config.Data.runRange = '262548'
+#config.Data.runRange = '262538-262735'
+config.Data.publication = True
+config.Data.outputDatasetTag = 'HIMinBias_v2'
+#config.Data.useParent = False
+#config.Site.storageSite = 'T2_CH_CERN'
+config.Site.storageSite = 'T2_US_MIT'
+
+
+### 1
+try:
+        crabCommand('submit', config = config)
+except HTTPException as hte:
+        print "Failed submitting task: %s" % (hte.headers)
+except ClientException as cle:
+        print "Failed submitting task: %s" % (cle)
+
+
+
+### 2
+config.General.requestName = 'HIMinimumBias2_HIRun2015-PromptReco-v2_262548-262768'
+config.Data.inputDataset = '/HIMinimumBias2/HIRun2015-PromptReco-v1/AOD'
+try:
+        crabCommand('submit', config = config)
+except HTTPException as hte:
+        print "Failed submitting task: %s" % (hte.headers)
+except ClientException as cle:
+        print "Failed submitting task: %s" % (cle)
+
+
+
+
