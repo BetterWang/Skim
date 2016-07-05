57925395d70a32125230e01721c9a69ae94ad545
Quan Wang <BetterWang@gmail.com>
2016-07-05 19:38:54 +0200

    [+] pixel

--- EventContent.py
+++ EventContent.py
@@ -43,3 +43,40 @@ analysisSkimQ2Debug = cms.PSet(
         'keep *_TriggerResults_*_*',
 	)
 )
+
+analysisSkimPixelContent = cms.PSet(
+        outputCommands = cms.untracked.vstring(
+        'drop *',
+        # event
+        'keep *_offlineBeamSpot_*_*',
+        'keep *_TriggerResults_*_*',
+        'keep L1GlobalTriggerReadoutRecord_gtDigis_*_*RECO',
+
+        # centrality
+        'keep *_hiCentrality_*_*',
+        'keep *_centralityBin_*_*',
+        'keep *_hiEvtPlane_*_*',
+
+        # digis
+        'keep ZDCDataFramesSorted_castorDigis_*_*',
+
+        # tower
+        'keep recoCastorTowers_CastorTowerReco_*_*',
+
+        # jet
+        'keep *_towerMaker_*_*',
+
+        # vertex
+        'keep *_hiSelectedVertex_*_*',
+        'keep *_hiClusterCompatibility_*_*',
+
+        # full tracks
+        'keep recoTracks_hiGeneralTracks_*_*',
+        'keep recoTracks_hiGeneralAndPixelTracks_*_*',
+
+        # pf
+        #'keep *_particleFlowTmp__*',
+        )
+)
+
+
---
+++ skimPixel.py
@@ -0,0 +1,62 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("SKIM")
+
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.MessageLogger.cerr.FwkReport.reportEvery = 100
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+
+from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '75X_dataRun2_v13', '')
+
+process.source = cms.Source ("PoolSource",
+                             fileNames = cms.untracked.vstring(
+                                     'file:../rereco/pixeltracking.root' #HIMinimumBias2
+                             )
+                            )
+
+process.maxEvents = cms.untracked.PSet(
+        input = cms.untracked.int32 (-1)
+)
+
+import HLTrigger.HLTfilters.hltHighLevel_cfi
+
+process.hltFilter = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone()
+process.hltFilter.HLTPaths = [
+                "HLT_HIL1MinimumBiasHF1AND_v*",
+                "HLT_HIL1MinimumBiasHF2AND_v*",
+                "HLT_HIL1MinimumBiasHF2AND_part*",
+]
+process.hltFilter.andOr = cms.bool(True)
+process.hltFilter.throw = cms.bool(False)
+
+
+process.load("RecoHI.HiCentralityAlgos.CentralityBin_cfi")
+process.centralityBin.Centrality = cms.InputTag("hiCentrality")
+process.centralityBin.centralityVariable = cms.string("HFtowers")
+
+
+process.load('HeavyIonsAnalysis.Configuration.collisionEventSelection_cff')
+process.clusterCompatibilityFilter.clusterPars = cms.vdouble(0.0,0.006)
+
+process.eventSelection = cms.Sequence(
+        process.hfCoincFilter3
+        + process.primaryVertexFilter
+        + process.clusterCompatibilityFilter
+)
+
+process.p = cms.Path(
+        process.hltFilter
+        * process.eventSelection
+        * process.centralityBin
+)
+
+import EventContent
+
+process.Out = cms.OutputModule("PoolOutputModule",
+                               SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring('p')),
+                               outputCommands = EventContent.analysisSkimPixelContent.outputCommands,
+                               fileName = cms.untracked.string ("pixelskim.root"),
+                              )
+
+process.out = cms.EndPath(process.Out)
